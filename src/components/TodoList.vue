c06f9d68dbe36f6249e29111d9fd8f68ffb4ef02
Springer <zxllks@yahoo.com>
2023-07-24 20:07:59 +0800

    L5 TodoList removeTodo

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -4,17 +4,14 @@ import { useTodoListStore } from '../stores/todoList'
 
 const store = useTodoListStore()
 const { todoList } = storeToRefs(store)
-const { toggleCompleted } = store
+const { toggleCompleted, removeTodo } = store
 </script>
 
 <template>
-  <div
-    v-for="{ item, id, completed } in todoList"
-    :key="id"
-    :class="{ completed }"
-    @click.stop="toggleCompleted(id)"
-  >
-    {{ item }}
+  <div v-for="{ item, id, completed } in todoList" :key="id">
+    <span :class="{ completed }"> {{ item }}</span>
+    <span @click.stop="toggleCompleted(id)">&#10004;</span>
+    <span @click.stop="removeTodo(id)">&#10060;</span>
   </div>
 </template>
 
