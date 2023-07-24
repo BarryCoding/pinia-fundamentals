551f7cc09717dee3e9bf76f3010e6f73d2c489d2
Springer <zxllks@yahoo.com>
2023-07-24 18:16:21 +0800

    L2 components and store setup

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,16 @@
+<script setup>
+import TodoForm from './TodoForm.vue';
+import TodoList from './TodoList.vue';
+
+
+</script>
+
+<template>
+  <div>
+    <h1>Todo List</h1>
+    <TodoForm />
+    <TodoList />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
