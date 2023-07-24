7f8a077e10b6170eba79dd3b84baf417a6106708
Springer <zxllks@yahoo.com>
2023-07-24 20:02:42 +0800

    L4 TodoList toggleCompleted

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,11 +1,25 @@
 <script setup>
+import { storeToRefs } from 'pinia'
+import { useTodoListStore } from '../stores/todoList'
 
+const store = useTodoListStore()
+const { todoList } = storeToRefs(store)
+const { toggleCompleted } = store
 </script>
 
 <template>
-  <div>
-
+  <div
+    v-for="{ item, id, completed } in todoList"
+    :key="id"
+    :class="{ completed }"
+    @click.stop="toggleCompleted(id)"
+  >
+    {{ item }}
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.completed {
+  text-decoration: line-through;
+}
+</style>
--- src/stores/todoList.js
+++ src/stores/todoList.js
@@ -7,10 +7,16 @@ export const useTodoListStore = defineStore('todoList', {
   }),
   actions: {
     addTodo(item) {
-      this.todoList.push({ item, id: this.id++, complete: false })
+      this.todoList.push({ item, id: this.id++, completed: false })
     },
     removeTodo(todoId) {
       this.todoList = this.todoList.filter((todo) => todo.id !== todoId)
+    },
+    toggleCompleted(todoId) {
+      const todo = this.todoList.find((todo) => todo.id === todoId)
+      if (todo) {
+        todo.completed = !todo.completed
+      }
     },
   },
 })
